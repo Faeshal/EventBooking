a2386288f84772c0bdd7821155c001d8ef6a8c86
faeshal <faeshal111@gmail.com>
2019-09-21 20:36:59 +0700

    fix bug date return value

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -8,6 +8,7 @@ const events = eventIds => {
       return events.map(event => {
         return {
           ...event._doc,
+          date: new Date(event._doc.date).toISOString(),
           creator: user.binds(this, event.creator)
         };
       });
@@ -28,4 +29,80 @@ const user = userId => {
     .catch(err => {
       throw err;
     });
+};
+
+module.exports = {
+  events: () => {
+    return Event.find()
+      .then(events => {
+        return events.map(event => {
+          return {
+            ...event._doc,
+            date: new Date(event._doc.date).toISOString(),
+            creator: user.bind(this, event._doc.creator)
+          };
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
+  createEvent: args => {
+    const event = new Event({
+      title: args.eventInput.title,
+      description: args.eventInput.description,
+      price: +args.eventInput.price,
+      date: new Date(args.eventInput.date),
+      creator: "5d84c23f2194890cdc1c9cca"
+    });
+    let createdEvent;
+    return event
+      .save()
+      .then(result => {
+        createdEvent = {
+          ...result._doc,
+          date: new Date(event._doc.date).toISOString(),
+          creator: user.bind(this, event._doc.creator)
+        };
+        return User.findById("5d84c23f2194890cdc1c9cca");
+        console.log(result);
+        return { ...result._doc };
+      })
+      .then(user => {
+        if (!user) {
+          throw new Eror("User Not Found");
+        }
+        user.createdEvents.push(event);
+        return user.save();
+      })
+      .then(result => {
+        return createdEvent;
+      })
+      .catch(err => {
+        console.log(err);
+        throw err;
+      });
+  },
+  createUser: args => {
+    return User.findOne({ email: args.userInput.email })
+      .then(user => {
+        if (user) {
+          throw new Error("Email Already Exist");
+        }
+        return bcrypt.hash(args.userInput.password, 12);
+      })
+      .then(hashedPassword => {
+        const user = new User({
+          email: args.userInput.email,
+          password: hashedPassword
+        });
+        return user.save();
+      })
+      .then(result => {
+        return { ...result._doc, password: null };
+      })
+      .catch(err => {
+        throw err;
+      });
+  }
 };
